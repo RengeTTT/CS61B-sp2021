85d7636cbc59c018b0b3a5ae03ab7e7f478f2814
RengeTTT <2878666472@qq.com>
2025-06-23 16:05:00 +0800

    feat(proj1):完成了proj的A & B

--- proj1/deque/Deque.java
+++ proj1/deque/Deque.java
@@ -1,5 +1,7 @@
 package deque;
 
+import java.util.Iterator;
+
 public interface Deque<T> extends Iterable<T> {
     void addFirst(T item);
 
@@ -21,4 +23,6 @@ public interface Deque<T> extends Iterable<T> {
 
     void printDeque();
 
+    @Override
+    Iterator<T> iterator();
 }
