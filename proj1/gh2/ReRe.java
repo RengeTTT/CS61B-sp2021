58f1be8ee79979a3c3c59a7f52c4ff1b8eaa8f02
RengeTTT <2878666472@qq.com>
2025-06-24 14:56:34 +0800

    fix(proj1):改进了proj1的style

--- proj1/gh2/ReRe.java
+++ proj1/gh2/ReRe.java
@@ -5,8 +5,7 @@ import java.io.*;
 public class ReRe {
 
     public static void main(String[] args) throws IOException {
-
-       GuitarPlayer player = new GuitarPlayer(new File("D:\\28596571465-1-192.mid"));
-       player.play();
+        GuitarPlayer player = new GuitarPlayer(new File("D:\\28596571465-1-192.mid\n"));
+        player.play();
     }
 }
